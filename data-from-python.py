711c1ad7b7549e86d48139a66b182169a98643f3
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-06-08 00:15:04 +0530

    Create data-from-python.py

---
+++ data-from-python.py
@@ -0,0 +1,19 @@
+
+
+# Open the CSV file
+with open("C:\\Users\\MAHENDER POSHABOINA\\Desktop\\m\\data.csv", "r") as file:
+    lines = file.readlines()
+    # Extract the data from the lines 
+    data = []
+    for line in lines:
+        values = line.strip().split(",")
+        data.append(values)
+for row in data[:5]:
+    print(row)
+
+    
+#output:
+# ['name', 'age', 'city']
+# ['Alice', '24', 'NewYork']
+# ['John', '32', 'Los Angeles']
+# ['Micheal', '44', 'Chicago']
