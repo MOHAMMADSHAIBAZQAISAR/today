fbd2ae38e5b7f8ff452b603c0a4bd8e127a12be1
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-06-09 00:25:14 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,56 +1,28 @@
-import csv
-import matplotlib.pyplot as plt
+# Sample data
+X = [1, 2, 3, 4, 5]  # Independent variable
+y = [2, 4, 6, 8, 10]  # Dependent variable
 
-# Static data
-data = [
-    ['Age', 'Income'],
-    [25, 25000],
-    [23, 22000],
-    [24, 26000],
-    [28, 29000],
-    [34, 38600],
-    [32, 36500],
-    [42, 41000],
-    [55, 81000],
-    [45, 47500]
-]
+# Calculate the means of X and y
+mean_x = sum(X) / len(X)
+mean_y = sum(y) / len(y)
 
-# Extracting age and income from the data
-age = [row[0] for row in data[1:]]
-income = [row[1] for row in data[1:]]
+# Calculate the deviations
+deviations_x = [xi - mean_x for xi in X]
+deviations_y = [yi - mean_y for yi in y]
 
-# Number of points
-num = len(age)
+# Calculate the slope (b1)
+numerator = sum(deviations_x[i] * deviations_y[i] for i in range(len(X)))
+denominator = sum(deviation_x ** 2 for deviation_x in deviations_x)
+b1 = numerator / denominator
 
-# Calculate mean of age and income
-mean_age = sum(age) / num
-mean_income = sum(income) / num
+# Calculate the intercept (b0)
+b0 = mean_y - b1 * mean_x
 
-# Calculate cross-deviation and deviation about age
-CD_ageincome = sum([x * y for x, y in zip(age, income)]) - num * mean_income * mean_age
-CD_ageage = sum([x ** 2 for x in age]) - num * mean_age ** 2
+# Make predictions
+X_new = [6, 7, 8]  # New data points to predict
+predictions = [b0 + b1 * xi for xi in X_new]
 
-# Calculate regression coefficients
-b1 = CD_ageincome / CD_ageage
-b0 = mean_income - b1 * mean_age
-
-# Display coefficients
-print("Estimated Coefficients:")
-print("b0 =", b0)
-print("b1 =", b1)
-
-# Plot the actual points as a scatter plot
-plt.scatter(age, income, color="b", marker="o")
-
-# Predict response vector
-response_vec = [b0 + b1 * x for x in age]
-
-# Plot the regression line
-plt.plot(age, response_vec, color="r")
-
-# Labels for the axes
-plt.xlabel('Age')
-plt.ylabel('Income')
-
-# Display the plot
-plt.show()
+# Print the coefficients and predictions
+print("Intercept:", b0)
+print("Coefficient:", b1)
+print("Predictions:", predictions)
