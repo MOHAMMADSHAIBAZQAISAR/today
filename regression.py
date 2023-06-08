ade6f6abba26e375632071c9e24294c6b6a2408f
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-06-08 22:12:28 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,36 +1,56 @@
-import pandas as pd
-import numpy as np
+import csv
 import matplotlib.pyplot as plt
-# To read data from Age_Income.csv file
-dataFrame = pd.read_csv('Age_Income.csv')
-# To place data in to age and income vectors
-age = dataFrame['Age']
-income = dataFrame['Income']
 
-# number of points
-num = np.size(age)
-# To find the mean of age and income vector
-mean_age = np.mean(age)
-mean_income = np.mean(income)
+# Static data
+data = [
+    ['Age', 'Income'],
+    [25, 25000],
+    [23, 22000],
+    [24, 26000],
+    [28, 29000],
+    [34, 38600],
+    [32, 36500],
+    [42, 41000],
+    [55, 81000],
+    [45, 47500]
+]
 
-# calculating cross-deviation and deviation about age
-CD_ageincome = np.sum(income*age) - num*mean_income*mean_age
-CD_ageage = np.sum(age*age) - num*mean_age*mean_age
+# Extracting age and income from the data
+age = [row[0] for row in data[1:]]
+income = [row[1] for row in data[1:]]
 
-# calculating regression coefficients
+# Number of points
+num = len(age)
+
+# Calculate mean of age and income
+mean_age = sum(age) / num
+mean_income = sum(income) / num
+
+# Calculate cross-deviation and deviation about age
+CD_ageincome = sum([x * y for x, y in zip(age, income)]) - num * mean_income * mean_age
+CD_ageage = sum([x ** 2 for x in age]) - num * mean_age ** 2
+
+# Calculate regression coefficients
 b1 = CD_ageincome / CD_ageage
-b0 = mean_income - b1*mean_age
-# to display coefficients
-print("Estimated Coefficients :")
-print("b0 = ",b0,"\nb1 = ",b1)
-# To plot the actual points as scatter plot
-plt.scatter(age, income, color = "b",marker = "o")
-# TO predict response vector
-response_Vec = b0 + b1*age
-# To plot the regression line
-plt.plot(age, response_Vec, color = "r")
-# Placing labels
+b0 = mean_income - b1 * mean_age
+
+# Display coefficients
+print("Estimated Coefficients:")
+print("b0 =", b0)
+print("b1 =", b1)
+
+# Plot the actual points as a scatter plot
+plt.scatter(age, income, color="b", marker="o")
+
+# Predict response vector
+response_vec = [b0 + b1 * x for x in age]
+
+# Plot the regression line
+plt.plot(age, response_vec, color="r")
+
+# Labels for the axes
 plt.xlabel('Age')
 plt.ylabel('Income')
-# To display plot
+
+# Display the plot
 plt.show()
